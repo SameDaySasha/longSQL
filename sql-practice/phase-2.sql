167a1604cf6b7b00664eeef264b3dea247105760
Alex Florea <alexflorea@alexs-mbp.myfiosgateway.com>
2023-04-12 19:42:22 -0400

    Phase1

--- sql-practice/phase-2.sql
+++ sql-practice/phase-2.sql
@@ -1,1 +1,23 @@
--- Your code here
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS coffee_orders;
+
+
+
+
+CREATE TABLE customers(
+
+    customer_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name VARCHAR(40), 
+    phone NUMERIC(10, 0),
+    email VARCHAR(255),
+    points INTEGER NOT NULL DEFAULT 5,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+);
+
+
+CREATE TABLE coffee_orders(
+    purchase_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    customer_id INTEGER REFERENCES customers(customer_id),
+    is_redeemed BOOLEAN DEFAULT 0,
+    ordered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
